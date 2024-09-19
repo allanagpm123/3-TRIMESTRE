dd3cb8854ace9ebd7a413026b1a1b486a90dcf68
allanagpm123 <132069655+allanagpm123@users.noreply.github.com>
2024-09-19 08:47:01 -0300

    Add files via upload

---
+++ PROJETO ED/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'REGRAS',
+    'Quantos jogadores compõem uma equipe de vôlei?',
+    'Cada equipe tem 6 jogadores em quadra.'
+)
+
+criaCartao(
+    'REGRAS',
+    'Qual é o número mínimo de toques que uma equipe pode fazer antes de passar a bola?',
+    'A equipe pode fazer até 3 toques.'
+)
+
+criaCartao(
+    'POSIÇÕES',
+    'Qual é a função do levantador no vôlei?',
+    'O levantador é responsável por fazer os passes para os atacantes, organizando as jogadas.'
+ )
+
+criaCartao(
+    'POSIÇÕES',
+    'Qual é o papel do líbero?',
+    'O líbero é um defensor especializado que não pode atacar e geralmente recebe as saídas de bola.'
+)
